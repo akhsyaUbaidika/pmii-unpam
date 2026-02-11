9ae82c55e5af3a1896422825106aa45a9b97a10b
akhsyaUbaidika <152408578+akhsyaUbaidika@users.noreply.github.com>
2026-02-11 13:41:14 +0700

    reset migrate

--- prisma/migrations/20260211064057_init/migration.sql
+++ prisma/migrations/20260211064057_init/migration.sql
@@ -13,8 +13,8 @@ CREATE TABLE `Article` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(191) NOT NULL,
     `slug` VARCHAR(191) NOT NULL,
-    `excerpt` TEXT NOT NULL,
-    `content` LONGTEXT NOT NULL,
+    `excerpt` VARCHAR(191) NOT NULL,
+    `content` TEXT NOT NULL,
     `image` VARCHAR(191) NOT NULL,
     `category` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
@@ -24,11 +24,27 @@ CREATE TABLE `Article` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Dokumentasi` (
+CREATE TABLE `Documentation` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(191) NOT NULL,
-    `image` VARCHAR(191) NOT NULL,
+    `slug` VARCHAR(191) NOT NULL,
+    `excerpt` VARCHAR(191) NOT NULL,
+    `content` TEXT NOT NULL,
+    `coverImage` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `Documentation_slug_key`(`slug`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `DocumentationImage` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `imageUrl` VARCHAR(191) NOT NULL,
+    `documentationId` INTEGER NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `DocumentationImage` ADD CONSTRAINT `DocumentationImage_documentationId_fkey` FOREIGN KEY (`documentationId`) REFERENCES `Documentation`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
