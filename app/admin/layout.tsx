588cea27b671c9b8e92c8705bf71bc0981a6bdd2
akhsyaUbaidika <152408578+akhsyaUbaidika@users.noreply.github.com>
2026-04-04 21:15:44 +0700

    phase 1 admin login fix 1

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,19 +1,25 @@
 "use client";
 
 import { useEffect } from "react";
+import { usePathname } from "next/navigation";
 
 export default function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+
   useEffect(() => {
     const token = localStorage.getItem("token");
+
+    // ❗ skip kalau di halaman login
+    if (pathname === "/admin/login") return;
 
     if (!token) {
       window.location.href = "/admin/login";
     }
-  }, []);
+  }, [pathname]);
 
   return <>{children}</>;
 }
